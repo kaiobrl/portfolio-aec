d173ed764f3d58365fb333f9612d43986577498a
KAIO_😎 <134008176+KaioxDev@users.noreply.github.com>
2025-03-30 16:53:49 -0300

    botao rola pra cima
    Aec2b

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function abrirModal(img) {
 function fecharModal() {
     document.getElementById('modal').style.display = 'none';
 }
+
+window.onscroll = function() {
+    let btnTopo = document.getElementById("btnTopo");
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btnTopo.style.display = "block";
+    } else {
+        btnTopo.style.display = "none";
+    }
+};
+
+function scrollParaTopo() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
